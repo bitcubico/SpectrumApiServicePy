39caf96384982cb4666cf0ef08703f338809d362
BITCUBICO Technology <bitcubico@gmail.com>
2021-03-03 17:12:46 -0500

    Actualizo los requerimientos y la versión

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = '1.0.2'
+VERSION = '1.0.2.2'
 DESCRIPTION = 'Es un paquete que tiene como propósito facilitar la comunicación con la API de SPECTRUM a través de ' \
               'Python. '
 
@@ -16,13 +16,13 @@ setup(
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bitcubico/SpectrumApiService",
+    url="https://github.com/bitcubico/SpectrumApiServicePy",
     packages=find_packages(),
     install_requires=[
-        'lxml~=4.6.2',
-        'requests~=2.25.1',
-        'setuptools~=53.0.0',
-        'log-helper-bit3~=1.2.0'
+        'lxml>=4.6.2',
+        'requests>=2.25.1',
+        'setuptools>=53.0.0',
+        'log-helper-bit3>=1.2.0'
     ],
     keywords=['python', 'spectrum', 'api', 'arus', 'mcubico'],
     classifiers=[
